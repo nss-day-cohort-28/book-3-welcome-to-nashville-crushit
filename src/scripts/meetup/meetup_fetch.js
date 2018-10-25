ccf3710e74be377e1a36dcea8adcf1cbb8d22c04
Elyse Dawson <dawson.elyse@gmail.com>
2018-10-25 10:51:47 -0500

    Added meetup categories to index.html. Fetch in meetupfetch.js is working.

--- src/scripts/meetup/meetup_fetch.js
+++ src/scripts/meetup/meetup_fetch.js
@@ -1,9 +1,18 @@
+// Eventbrite Anonymous access OAuth token BGQCNORPETDNTE2EVJT2
+
+
+const foodDrink = "110";
+const arts = "105";
+const sports = "108";
+const hobbies = "119";
+const familyEducation = "115";
+
 fetch("https://www.eventbriteapi.com/v3/events/search/?location.latitude=36.174465&location.longitude=-86.767960&sort_by=date", {
   headers: {
     "Authorization": "Bearer BGQCNORPETDNTE2EVJT2"
   }
-    .then(meetup => meetup.json())
-    .then(filtered_meetups => {
-      console.table(filtered_meetups)
-    })
 })
+  .then(meetup => meetup.json())
+  .then(filtered_meetups => {
+    console.log(filtered_meetups)
+  })
