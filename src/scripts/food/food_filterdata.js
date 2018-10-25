f0b27dbf3c3dd479cfcae690cb47d1c2109e0c0f
jasehackman <jasehackman@gmail.com>
2018-10-25 14:23:21 -0500

    added comments to make it easier to read

--- src/scripts/food/food_filterdata.js
+++ src/scripts/food/food_filterdata.js
@@ -1,13 +1,17 @@
+// captured the array stored in the Restaurants object in a varable
 const foodFilter = (foodObj) => {
   let restArray = foodObj.restaurants;
   return restArray;
 }
 
+// Loop through my array and collect the name and location of each restaurant. T
 const foodArrayLoop = (newArray) => {
   for (let i = 0; i<5; i++) {
     let restName = newArray[i].restaurant.name;
     let restLocation = newArray[i].restaurant.location.address;
+    // insert name and location into our build component
     elementBuilder(restName, restLocation);
   }
+  // inject our build component into the dom
   injection();
 } 
