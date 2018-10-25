ff1cd77b8e57ba4881d470ba27007382350669cc
jasehackman <jasehackman@gmail.com>
2018-10-25 14:02:07 -0500

    got my filtered data to post to the dom

--- src/scripts/food/food_filterdata.js
+++ src/scripts/food/food_filterdata.js
@@ -6,8 +6,8 @@ const foodFilter = (foodObj) => {
 const foodArrayLoop = (newArray) => {
   for (let i = 0; i<5; i++) {
     let restName = newArray[i].restaurant.name;
-    console.log(restName);
     let restLocation = newArray[i].restaurant.location.address;
-    console.log(restLocation);
+    elementBuilder(restName, restLocation);
   }
+  injection();
 } 
