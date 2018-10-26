07641744fa4cd9dc8655226232ce67939b40e4a1
jasehackman <jasehackman@gmail.com>
2018-10-26 14:06:41 -0500

    made it so the search button would also update json and reload page

--- src/scripts/local_storage.js
+++ src/scripts/local_storage.js
@@ -5,10 +5,10 @@ const findItinerary = document.getElementById("itinerary");
 // function to create itinerary div
 let itineraryBuilder = (jsonObj) => {
   return `<div class = "inserted">
-    <h3>Concert: ${jsonObj.music}</h3>
+    <h3>Music: ${jsonObj.music}</h3>
     <h3>Park: ${jsonObj.park}</h3>
-    <h3>Event: ${jsonObj.meetup}</h3>
-    <h3>Restaurant: ${jsonObj.food}</h3>
+    <h3>Meetup: ${jsonObj.meetup}</h3>
+    <h3>Food: ${jsonObj.food}</h3>
   </div>`;
 }
 
@@ -26,18 +26,32 @@ fetch(`http://localhost:8088/my_itinerary`, {
 
 let radbut = document.getElementById("button_save")
 
+let bigObj = {
+
+}
+
+const localJson = function (data) {
+fetch(`http://localhost:8088/my_itinerary`, {
+method: "PATCH",
+headers: {
+"Content-Type": "application/json",
+},
+body: JSON.stringify(data)})}
+
 radbut.addEventListener("click", function () {
   let radioCheck = document.querySelector('input[name="result"]:checked').value;
   let radioClass = document.querySelector('input[name="result"]:checked').className;
-  console.log(radioCheck)
-  console.log(radioClass)
+  if (radioClass === "music") {
+    bigObj.music = radioCheck
+  } else if (radioClass === "meetup") {
+    bigObj.meetup = radioCheck
+  } else if (radioClass === "park") {
+    bigObj.park = radioCheck
+  } else if (radioClass === "food") {
+    bigObj.food = radioCheck
+  }
+  localJson(bigObj);
+  window.location.reload();
 })
 
-// fetch(`http://localhost:8088/my_itinerary`, {
-//   headers: {
-//     "Content-Type": "application/json",
-//   }
-// }).then(data => data.json())
-// .then((data) => {
-//   findItinerary.insertAdjacentHTML('beforeend',itineraryBuilder(data));
-// })
+
